72987bc775f76e9b2271e78d7c8d6ec9edf843e5
Stilyan Petrov <f.and.g.2019@gmail.com>
2025-12-21 18:08:44 +0200

    Create repositories

---
+++ src/main/java/Santas_Workshop_API/repository/GiftsRepository.java
@@ -0,0 +1,9 @@
+package Santas_Workshop_API.repository;
+
+import Santas_Workshop_API.entity.Gift;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GiftsRepository extends JpaRepository<Gift, Integer> {
+}
